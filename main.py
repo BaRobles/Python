f3ec9179f9d1b3cf026de134085706a9171a4326
BaRobles <23910427-BaRobles@users.noreply.replit.com>
2023-06-13 17:41:37 +0000

    Initial Commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from random import randint
+
+while True:
+
+  attempts = 0
+  num = randint(1, 25)
+  
+  while attempts < 6:
+  
+    x = int(input("Try to guess! Enter a number between 1 or 25: "))
+  
+    if(num < x): 
+      print("Too high!")
+      
+    elif(num > x): 
+      print("Too low!")
+      
+    else: 
+      print("Correct!")
+      break
+  
+    attempts += 1
+  
+  if attempts == 6:
+    print(f"The correct answer is: {num}")
